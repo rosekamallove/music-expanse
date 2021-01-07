0f4246fed086c94f6c51030ea8f16b3dd7a3d50c
rosekamallove <rosekamallove@gmail.com>
2021-01-07 12:46:30 +0530

    Initial commit

--- music_room/api/serializers.py
+++ music_room/api/serializers.py
@@ -10,3 +10,9 @@ class RoomSerializer(serializers.ModelSerializer):
         # these are all the properties in the room class (model)
         fields = ('id', 'code', 'host', 'guest_can_pause',
                   'votes_to_skip', 'created_at',)
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
--- music_room/api/views.py
+++ music_room/api/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 # this will allow us to create a class that inherits from a generic API view
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import RoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 
 
